ccc879db0d6e7354402c9c8c5e3268f331a337c0
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-1615.local>
2022-06-11 11:39:27 -0400

    Added Apollo Client

---
+++ src/store/reducers/index.ts
@@ -0,0 +1,35 @@
+import { combineReducers } from "redux";
+import { EDefaultActions } from "../constants/index";
+import { IAction } from "../interfaces/action.interface";
+import { authReducer, IAuthReducer } from "./auth.reducer";
+
+export interface IRootReducer {
+    default: IDefaultReducer,
+    auth: IAuthReducer,
+}
+
+interface IDefaultReducer {
+    loading: boolean,
+}
+
+const initialState : IDefaultReducer = {
+    loading: false,
+};
+
+const defaultReducer = (state:IDefaultReducer=initialState, action:IAction) : IDefaultReducer => {
+    switch(action.type) {
+        case EDefaultActions.SET_LOADING: {
+            return { ...state, loading: action.payload };
+        }
+        default: {
+            return state; 
+        }
+    }
+};
+
+const reducers = combineReducers({
+    default: defaultReducer,
+    auth: authReducer,
+});
+
+export { reducers };
